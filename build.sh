0bc0be87b42ea0cebe29b494b5463deb6773560c
Emma Marichal <bonjour@emmamarichal.fr>
2022-03-04 12:18:36 +0100

    Build.sh modified

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ gftools builder sources/config.yaml
 desktop=$(ls fonts/ttf/*.ttf)
 for font in $desktop
 do
-  pyftsubset $font --unicodes-file=sources/glyphset.txt --layout-features='*' --notdef-outline  --name-languages='*' --name-IDs='*' --drop-tables=
+  pyftsubset $font --unicodes-file=sources/glyphset.txt --glyph-names --drop-tables=  --layout-features+='aalt','lnum','case','dlig','zero','subs','sinf','sups','dnom' --layout-features-='smcp','c2sc','calt' --recommended-glyphs --notdef-outline --symbol-cmap --legacy-cmap --name-IDs='*' --name-legacy --name-languages='*'
   rm $font
 
 done
